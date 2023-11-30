1b30bbc0663d127cfb3bb84314e55a1c67d8fb54
Vinay89824 <s565587@nwmissouri.edu>
2023-11-29 22:06:02 -0600

    updated the file

--- app.js
+++ app.js
@@ -3,90 +3,124 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-
+ 
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+ 
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+  Account.findOne({ username: username })
+  .then(function (user){
+  if (err) { return done(err); }
+  if (!user) {
+  return done(null, false, { message: 'Incorrect username.' });
+  }
+  if (!user.validPassword(password)) {
+  return done(null, false, { message: 'Incorrect password.' });
+  }
+  return done(null, user);
+  })
+  .catch(function(err){
+  return done(err)
+  })
+  })
+ )
+ 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var gadgetRouter = require('./routes/gadget');
 var boardRouter = require('./routes/board');
 var chooseRouter = require('./routes/choose');
-var gadget= require('./models/gadget');
+var gadget = require("./models/gadget");
+ 
 var resourceRouter = require('./routes/resource');
-
-require('dotenv').config();
-const connectionString =
-process.env.MONGO_CON
-mongoose = require('mongoose');
-mongoose.connect(connectionString);
-
-//Get the default connection
-var db = mongoose.connection;
-//Bind connection to error event
-db.on('error', console.error.bind(console, 'MongoDB connectionerror:'));
-db.once("open", function(){
-console.log("Connection to DB succeeded")});
-
-// We can seed the collection if needed on
-async function recreateDB(){
-// Delete everything
-await gadget.deleteMany();
-let instance1 = new
-gadget({name:"mobile", quantity:"one",price:20});
-instance1.save().then(doc=>{
-console.log("First object saved")}
-).catch(err=>{
-console.error(err)
-});
-let instance2 = new
-gadget({name:"refrigirator", quantity:"three",price:30});
-instance2.save().then(doc=>{
-console.log("second object saved")}
-).catch(err=>{
-console.error(err)
-});
-let instance3 = new
-gadget({name:"laptop", quantity:"two",price:15});
-instance3.save().then(doc=>{
-console.log("third object saved")}
-).catch(err=>{
-console.error(err)
-});
-}
-let reseed = true;
-if (reseed) {recreateDB();}
-
 var app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-
+ 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+ }));
+ app.use(passport.initialize());
+ app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
-
+ 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/gadget', gadgetRouter);
 app.use('/board', boardRouter);
 app.use('/choose', chooseRouter);
 app.use('/resource', resourceRouter);
+ 
+ 
+require('dotenv').config();
+const connectionString =
+process.env.MONGO_CON
+mongoose = require('mongoose');
+mongoose.connect(connectionString);
+ 
+var db = mongoose.connection;
+//Bind connection to error event
+db.on('error', console.error.bind(console, 'MongoDB connectionerror:'));
+db.once("open", function(){
+console.log("Connection to DB succeeded")});
+ 
+async function recreateDB(){
+  // Delete everything
+  await gadget.deleteMany();
+  let instance1 = new
+  gadget({name:"mobile", quantity:"one",price:20});
+  instance1.save().then(doc=>{
+  console.log("First object saved")}
+  ).catch(err=>{
+  console.error(err)
+  });
+  let instance2 = new
+  gadget({name:"refrigirator", quantity:"three",price:30});
+  instance2.save().then(doc=>{
+  console.log("second object saved")}
+  ).catch(err=>{
+  console.error(err)
+  });
+  let instance3 = new
+  gadget({name:"laptop", quantity:"two",price:15});
+  instance3.save().then(doc=>{
+  console.log("third object saved")}
+  ).catch(err=>{
+  console.error(err)
+  });
+  }
+  let reseed = true;
+  if (reseed) {recreateDB();}
+ 
+var Account =require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
+ 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
-
+ 
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
+ 
   // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
-
+ 
+ 
+ 
 module.exports = app;
---
+++ models/account.js
@@ -0,0 +1,14 @@
+// passport config
+// Use the existing connection
+// The Account model
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const passportLocalMongoose = require("passport-local-mongoose");
+const accountSchema = new Schema({
+ username: String,
+ password: String
+});
+accountSchema.plugin(passportLocalMongoose);
+// We export the Schema to avoid attaching the model to the
+// default mongoose connection.
+module.exports = mongoose.model("Account", accountSchema);
