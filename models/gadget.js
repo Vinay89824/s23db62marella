4c23c5b089a2a87205fa36f07c57671b3f8a8e4a
Vinay89824 <s565587@nwmissouri.edu>
2023-11-29 22:37:31 -0600

    added buttons

--- models/gadget.js
+++ models/gadget.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose")
 const gadgetSchema = mongoose.Schema({
 name: String,
 quantity: String,
-price: Number
+price: { type : Number,
+    min : 1,
+    max : 40000
+},
 })
 module.exports = mongoose.model("gadget",
 gadgetSchema)
