45202def5961e6b115a879127e2b16f6ff3a6fbf
Vinay89824 <s565587@nwmissouri.edu>
2023-11-29 22:21:09 -0600

    updated the file

--- routes/gadget.js
+++ routes/gadget.js
@@ -1,13 +1,22 @@
 var express = require('express');
 const gadget_controlers= require('../controllers/gadget');
 var router = express.Router();
+// A little function to check if we have an authorized user and continue on 
+//or
+// redirect to login.
+const secured = (req, res, next) => {
+ if (req.user){
+ return next();
+ }
+ res.redirect("/login");
+ }
 router.get('/', gadget_controlers.gadget_view_all_Page );
 /* GET detail gadget page */
 router.get('/detail', gadget_controlers.gadget_view_one_Page);
 /* GET create gadget page */
 router.get('/create', gadget_controlers.gadget_create_Page);
 /* GET create update page */
-router.get('/update', gadget_controlers.gadget_update_Page);
+router.get('/update', secured,gadget_controlers.gadget_update_Page);
 /* GET delete gadget page */
 router.get('/delete', gadget_controlers.gadget_delete_Page);
 
